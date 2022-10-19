ddac726eac4c0b3f116241f6d960e0b3298709a7
abhishtagatya <abhishtagatya@yahoo.com>
2022-10-19 19:57:28 +0700

    Initial Commit : Hackathon Submit

---
+++ main.py
@@ -0,0 +1,127 @@
+import json
+import random
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+import whisper
+
+import streamlit as st
+
+from model.ser_model import SER
+
+model = whisper.load_model("base")
+ser_model = SER.load('pretrained/cnn_attention_lstm_model.pt')
+
+with open('safewords.json', 'r') as swfile:
+    swfile_json = json.load(swfile)
+
+
+def detect_safe_words(full_text: str, safe_words: List[str]):
+    tokens = full_text.lower().split()
+
+    flag = False
+    for token in tokens:
+        for word in safe_words:
+            if token == word.lower():
+                flag = True
+
+    if flag:
+        st.session_state.on_alert = True
+        return
+    st.session_state.on_alert = False
+
+
+def detect_emotion(model, audio_file, match_emotion):
+    if st.session_state.on_alert:
+        return
+
+    prediction_bool = model.match_prediction(audio_file, match_emotion)
+
+    if prediction_bool:
+        st.session_state.on_alert = True
+        return
+    st.session_state.on_alert = False
+
+
+def spacing(units=1):
+    for _ in range(units):
+        st.write("")
+
+
+st.set_page_config(layout="wide", page_icon="🚨", page_title="SafeWord - Powered by OpenAI Whisper")
+
+if "working_text" not in st.session_state:
+    # set the initial default value of the slider widget
+    st.session_state.working_text = ""
+
+if "on_alert" not in st.session_state:
+    # set the initial default value of the slider widget
+    st.session_state.on_alert = False
+
+if "audio_file_uploaded" not in st.session_state:
+    st.session_state.audio_file_uploaded = False
+
+st.title("🚨 SafeWord")
+
+st.markdown("""
+        Utilizing OpenAI's Whisper model and a CNN-Based Speech Emotion Recognition (SER)
+        model to determine whether to call the authorities based on sentiment. 
+        """)
+spacing(1)
+
+safeword_choices = st.multiselect(
+    "Choose a Rarely Used Word",
+    swfile_json['wordlist'],
+    swfile_json['wordlist'][0:2]
+)
+
+uploaded_file = st.file_uploader("Put Audio File", type=['mp3', 'm4a', 'wav'])
+
+if uploaded_file is not None:
+    print(uploaded_file.name)
+
+    decoding_progress = 0
+    decoding_progress_bar = st.progress(decoding_progress)
+
+    audio_file = uploaded_file.read()
+
+    with open('temp.mp3', 'wb') as temp_audio:
+        temp_audio.write(audio_file)
+
+    decoding_progress += 10
+    decoding_progress_bar.progress(decoding_progress)
+
+    whisper_audio = whisper.load_audio('temp.mp3')
+    whisper_audio = whisper.pad_or_trim(whisper_audio)
+
+    decoding_progress += 20
+    decoding_progress_bar.progress(decoding_progress)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(whisper_audio).to(model.device)
+
+    decoding_progress += 30
+    decoding_progress_bar.progress(decoding_progress)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+
+    decoding_progress += 40
+    decoding_progress_bar.progress(decoding_progress)
+
+    print(result.text)
+    detect_safe_words(full_text=result.text, safe_words=safeword_choices)
+    detect_emotion(ser_model, 'temp.mp3', [5, 6, 7, 0])
+
+    st.audio(audio_file, format='audio/ogg')
+    decoding_progress_bar.empty()
+
+if st.session_state.on_alert:
+    st.error("The Police has been Alerted, Please Stay Safe until they Arrive", icon="🚨")
---
+++ model/ser_model.py
@@ -0,0 +1,239 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+import librosa
+
+from sklearn.preprocessing import StandardScaler
+
+
+class TimeDistributed(nn.Module):
+
+    def __init__(self, module):
+        super(TimeDistributed, self).__init__()
+        self.module = module
+
+    def forward(self, x):
+        if len(x.size()) <= 2:
+            return self.module(x)
+        elif len(x.size()) == 3:
+            x_reshape = x.contiguous().view(-1, x.size(2))
+        elif len(x.size()) == 4:
+            x_reshape = x.contiguous().view(-1, x.size(2), x.size(3))
+        else:
+            x_reshape = x.contiguous().view(-1, x.size(2), x.size(3), x.size(4))
+
+        y = self.module(x_reshape)
+
+        if len(x.size()) == 3:
+            y = y.contiguous().view(x.size(0), -1, y.size(1))
+        elif len(x.size()) == 4:
+            y = y.contiguous().view(x.size(0), -1, y.size(1), y.size(2))
+        else:
+            y = y.contiguous().view(x.size(0), -1, y.size(1), y.size(2), y.size(3))
+        return y
+
+
+class HybridModel(nn.Module):
+
+    def __init__(self, num_emotions):
+        super().__init__()
+
+        self.conv2d_block = nn.Sequential(
+            # 1st Conv Block
+            TimeDistributed(
+                nn.Conv2d(
+                    in_channels=1,
+                    out_channels=16,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1
+                )
+            ),
+            TimeDistributed(nn.BatchNorm2d(16)),
+            TimeDistributed(nn.ReLU()),
+            TimeDistributed(nn.MaxPool2d(kernel_size=2, stride=2)),
+            TimeDistributed(nn.Dropout(0.3)),
+
+            # 2nd Conv Block
+            TimeDistributed(
+                nn.Conv2d(
+                    in_channels=16,
+                    out_channels=32,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1
+                )
+            ),
+            TimeDistributed(nn.BatchNorm2d(32)),
+            TimeDistributed(nn.ReLU()),
+            TimeDistributed(nn.MaxPool2d(kernel_size=4, stride=4)),
+            TimeDistributed(nn.Dropout(0.3)),
+
+            # 3rd Conv Block
+            TimeDistributed(
+                nn.Conv2d(
+                    in_channels=32,
+                    out_channels=64,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1
+                )
+            ),
+            TimeDistributed(nn.BatchNorm2d(64)),
+            TimeDistributed(nn.ReLU()),
+            TimeDistributed(nn.MaxPool2d(kernel_size=4, stride=4)),
+            TimeDistributed(nn.Dropout(0.3)),
+        )
+
+        # LSTM Block
+        hidden_size = 64
+        self.lstm = nn.LSTM(input_size=1024, hidden_size=hidden_size, bidirectional=True, batch_first=True)
+        self.dropout_lstm = nn.Dropout(0.4)
+        self.attention_linear = nn.Linear(2 * hidden_size, 1)
+
+        # Linear Softmax Layer
+        self.out_linear = nn.Linear(2 * hidden_size, num_emotions)
+
+    def forward(self, x):
+        conv_emb = self.conv2d_block(x)
+        conv_emb = torch.flatten(conv_emb, start_dim=2)
+        lstm_emb, (h, c) = self.lstm(conv_emb)
+        lstm_emb = self.dropout_lstm(lstm_emb)
+
+        batch_size, T, _ = lstm_emb.shape
+        attention_weights = [None] * T
+
+        for t in range(T):
+            emb = lstm_emb[:, t, :]
+            attention_weights[t] = self.attention_linear(emb)
+
+        attention_weights_norm = nn.functional.softmax(torch.stack(attention_weights, -1), dim=-1)
+        attention = torch.bmm(attention_weights_norm, lstm_emb)
+        attention = torch.squeeze(attention, 1)
+        output_logits = self.out_linear(attention)
+        output_softmax = nn.functional.softmax(output_logits, dim=1)
+
+        return output_logits, output_softmax, attention_weights_norm
+
+
+def loss_fn(predictions, targets):
+    return nn.CrossEntropyLoss()(input=predictions, target=targets)
+
+
+class SER:
+    EMOTIONS = {
+        1: 'neutral',
+        2: 'calm',
+        3: 'happy',
+        4: 'sad',
+        5: 'angry',
+        6: 'fear',
+        7: 'disgust',
+        0: 'surprised'
+    }
+    SAMPLE_RATE = 48000
+
+    def __init__(self, mode='train', model_path=''):
+        self.mode = mode
+
+        self.model = HybridModel
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        self.model_uc = self.model(len(self.EMOTIONS)).to(self.device)
+
+        if self.mode == 'inference':
+            if self.device == 'cuda':
+                self.model_uc.load_state_dict(state_dict=torch.load(model_path))
+            else:
+                self.model_uc.load_state_dict(state_dict=torch.load(model_path, map_location=torch.device('cpu')))
+            print('Loaded SER Model')
+
+    @staticmethod
+    def get_mel_spectrogram(audio, sample_rate):
+        mel_spec = librosa.feature.melspectrogram(
+            y=audio, sr=sample_rate, n_fft=1024, win_length=512,
+            window='hamming', hop_length=256, n_mels=128, fmax=sample_rate / 2
+        )
+        mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
+        return mel_spec_db
+
+    @staticmethod
+    def split_into_chunks(mel_spec, win_size, stride):
+        t = mel_spec.shape[1]
+        num_of_chunks = int(t / stride)
+
+        chunks = []
+        for i in range(num_of_chunks):
+            chunk = mel_spec[:, i * stride:i * stride + win_size]
+            if chunk.shape[1] == win_size:
+                chunks.append(chunk)
+        return np.stack(chunks, axis=0)
+
+    @classmethod
+    def load(cls, model_path):
+        return cls(mode='inference', model_path=model_path)
+
+    def predict(self, audio_file, sr=SAMPLE_RATE):
+        audio, sample_rate = librosa.load(audio_file, sr=sr)
+
+        mel_audio = self.get_mel_spectrogram(audio, sample_rate=sr)
+
+        mel_audio_chunked = []
+        chunks = self.split_into_chunks(mel_audio, win_size=128, stride=64)
+        mel_audio_chunked.append(chunks)
+
+        X_audio = np.stack(mel_audio_chunked, axis=0)
+        X_audio = np.expand_dims(X_audio, 2)
+
+        scaler = StandardScaler()
+
+        b, t, c, h, w = X_audio.shape
+        X_audio = np.reshape(X_audio, newshape=(b, -1))
+        X_audio = scaler.fit_transform(X_audio)
+        X_audio = np.reshape(X_audio, newshape=(b, t, c, h, w))
+
+        X_audio_tensor = torch.tensor(X_audio, device=self.device).float()
+
+        with torch.no_grad():
+            output_logits, output_softmax, attention_weights_norm = self.model_uc(X_audio_tensor)
+            print(output_softmax)
+            predictions = torch.argmax(output_softmax, dim=1)
+            print(predictions, output_softmax)
+
+    def match_prediction(self, audio_file, match_result, sr=SAMPLE_RATE):
+        duration = int(librosa.get_duration(filename=audio_file, sr=sr))
+        audio, sample_rate = librosa.load(audio_file, duration=duration, sr=sr)
+
+        signal = np.zeros((int(sr * duration, )))
+        signal[:len(audio)] = audio
+
+        mel_audio = self.get_mel_spectrogram(signal, sample_rate=sr)
+
+        mel_audio_chunked = []
+        chunks = self.split_into_chunks(mel_audio, win_size=128, stride=64)
+        mel_audio_chunked.append(chunks)
+
+        X_audio = np.stack(mel_audio_chunked, axis=0)
+        X_audio = np.expand_dims(X_audio, 2)
+
+        scaler = StandardScaler()
+
+        b, t, c, h, w = X_audio.shape
+        X_audio = np.reshape(X_audio, newshape=(b, -1))
+        X_audio = scaler.fit_transform(X_audio)
+        X_audio = np.reshape(X_audio, newshape=(b, t, c, h, w))
+
+        X_audio_tensor = torch.tensor(X_audio, device=self.device).float()
+
+        with torch.no_grad():
+            output_logits, output_softmax, attention_weights_norm = self.model_uc(X_audio_tensor)
+            print(output_softmax)
+            predictions = torch.argmax(output_softmax, dim=1)
+
+        print(predictions[0], type(predictions))
+        if predictions in match_result:
+            return True
+        return False
+
+
